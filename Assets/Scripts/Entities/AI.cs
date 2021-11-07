a11dc4274968e20005656c9a233a9eb6aa700e22
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2021-11-07 00:32:00 -0400

    Improved cutscene, AI state machine. Minor additional fixes.
    AI state machine now use an interface.
    
    Player controls is now an AI state (allowing for custscene character control).
    
    Human prefabs for reuse.
    
    Minor fix to A* (seems to be better at giving shorest path).
    
    Other bug fixes (too many to list).

--- Assets/Scripts/Entities/AI.cs
+++ Assets/Scripts/Entities/AI.cs
@@ -2,60 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class AI : AbstractHuman {
-    protected AIState state;
-    private TilemapGraph graph;
-    private float reachDistance = 0.025f;
-
-    public override void Start() {
+public abstract class AI : AbstractHuman,  {
+    protected override void Start() {
         base.Start();
-        this.graph = FindObjectOfType<TilemapGraph>();
-        this.state = new LookAtState(this, Vector2.down);
-        this.state.Enter();
 
-        EventManager.Get().Subscribe((StartEvent startEvent) => OnStartGame(startEvent));
     }
 
-    private void OnStartGame(StartEvent startEvent) {
-        this.state = new IdleState(this);
-    }
 
-    public TilemapGraph GetGraph() {
-        return this.graph;
-    }
-
-    public bool ReachedPosition(Vector3 target) {
-        return Vector2.Distance(target, this.transform.position) < this.reachDistance;
-    }
-
-    public void MoveTowards(Vector3 target) {
-        this.direction = new Vector2(target.x, target.y) - new Vector2(this.transform.position.x, this.transform.position.y);
-        this.direction.Normalize();
-
-        this.transform.position += new Vector3(this.direction.x, this.direction.y, 0) * Time.deltaTime * this.speed;
-    }
-
-    public void ResetDirection() {
-        this.direction = new Vector2();
-        this.spriteRenderer.sprite = this.sprites[0];
-        this.spriteRenderer.flipX = false;
-    }
-
-    public AIState GetState() {
-        return this.state;
-    }
-
-    protected void SetNextState(AIState nextState) {
-        if(nextState == this.state) return;
-        this.state.Exit();
-        this.state = nextState;
-        this.state.Enter();
-    }
-
-    public override void Update() {
-        base.Update();
-
-        this.state.Update();
-        this.SetNextState(this.state.NextState());
-    }
 }
