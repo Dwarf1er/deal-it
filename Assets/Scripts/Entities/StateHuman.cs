fd65df09d3f4c28d0527ebf5cc7e0df4498dbbc2
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2021-11-08 06:24:38 -0500

    Fixed LookAt.

--- Assets/Scripts/Entities/StateHuman.cs
+++ Assets/Scripts/Entities/StateHuman.cs
@@ -67,7 +67,7 @@ public abstract class StateHuman : MonoBehaviour, ISubscriber, IWithPosition, IW
     }
 
     private int GetDirectionIndex(Vector2 direction) {
-        if(direction.magnitude < 0.01f) return -1;
+        if(direction.magnitude == 0) return -1;
 
         float angle = Vector2.SignedAngle(Vector2.right, direction) + 180.0f;
 
@@ -85,7 +85,7 @@ public abstract class StateHuman : MonoBehaviour, ISubscriber, IWithPosition, IW
     public void LookAt(Vector2 position) {
         Vector2 displacement = position - (Vector2)this.transform.position;
 
-        if(direction.magnitude == 0) return;
+        if(displacement.magnitude == 0) return;
 
         LookTowards(displacement.normalized);
     }
