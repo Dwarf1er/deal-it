34f1293983e5f8375b0fbacdda8668e91ab9b66e
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2021-11-17 14:59:02 -0500

    Fixed character selection.
    Using proper input for selecting character.
    
    Fixed offset index.

--- Assets/Scripts/UI/CharacterSelection.cs
+++ Assets/Scripts/UI/CharacterSelection.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CharacterSelection : MonoBehaviour
-{
+public class CharacterSelection : MonoBehaviour, ISubscriber {
     public string[] characterSprites;
     public GameObject humanPrefab;
     public Vector2 center;
@@ -10,6 +9,7 @@ public class CharacterSelection : MonoBehaviour
     private Vector2 offset = new Vector2(0.32f, 0);
     private GameObject[] characters;
     private int selectedCharacterIndex;
+    private bool keyDown = false;
 
     private void Start() {
         characters = new GameObject[characterSprites.Length];
@@ -22,17 +22,55 @@ public class CharacterSelection : MonoBehaviour
                 selectedCharacterIndex = i;
             }
         }
+
+        EventManager.Get()
+            .Subscribe((MoveInputEvent inputEvent) => OnMove(inputEvent))
+            .Subscribe((DealInputEvent inputEvent) => OnSelect(inputEvent));
+    }
+
+    private void OnDestroy() {
+        EventManager.Get().UnSubcribeAll(this);
+    }
+
+    private void OnMove(MoveInputEvent inputEvent) {
+        if(inputEvent.direction.x == 0) {
+            keyDown = false;
+            return;
+        }
+
+        if(keyDown) return;
+        keyDown = true;
+
+        if(inputEvent.direction.x > 0) {
+            selectedCharacterIndex = (selectedCharacterIndex + 1) % characterSprites.Length;
+        } else {
+            selectedCharacterIndex = (selectedCharacterIndex - 1);
+
+            if(selectedCharacterIndex < 0) selectedCharacterIndex = characterSprites.Length - 1;
+        }
+
+        PlayerPrefs.SetString("Skin", characterSprites[selectedCharacterIndex]);
+        PlayerPrefs.Save();
+    }
+
+    private void OnSelect(DealInputEvent inputEvent) {
+        SceneManager.LoadScene("Cutscene");
+    }
+
+    public bool HasDistance() {
+        return false;
+    }
+
+    public Transform GetTransform() {
+        return transform;
     }
 
     private void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            selectedCharacterIndex = (selectedCharacterIndex + 1) % characterSprites.Length;
-            PlayerPrefs.SetString("Skin", characterSprites[selectedCharacterIndex]);
-            PlayerPrefs.Save();
-        }
-
         for (int i = 0; i < characterSprites.Length; i++) {
-            int positionIndex = (i + selectedCharacterIndex) % characters.Length;
+            int positionIndex = (selectedCharacterIndex - i + characterSprites.Length / 2) % characterSprites.Length;
+
+            if(positionIndex < 0) positionIndex += characterSprites.Length;
+
             Vector3 targetPosition = (center + offset * (positionIndex - characterSprites.Length / 2));
             
             if (Vector3.Distance(targetPosition, characters[i].transform.position) > 0.01f) {
@@ -45,13 +83,9 @@ public class CharacterSelection : MonoBehaviour
                 targetScale = new Vector3(2, 2, 1);
             }
 
-            if (Vector3.Distance(targetScale, characters[i].transform.localScale) > 0.01f) {
+            if (Vector3.Distance(targetScale, characters[i].transform.localScale) > 0.1f) {
                 characters[i].transform.localScale += (targetScale - characters[i].transform.localScale).normalized * 10f * Time.deltaTime;
             }
-        }
-
-        if (Input.GetKeyDown(KeyCode.E)) {
-            SceneManager.LoadScene("Cutscene");
         }
     }
 }
